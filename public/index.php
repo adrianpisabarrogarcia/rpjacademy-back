343cda6a55fc13bb5451c40d875fd7d20b63c0a1
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2023-08-09 18:16:58 +0200

    Add CORS configuration

--- public/index.php
+++ public/index.php
@@ -3,11 +3,22 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Tuupola\Middleware\CorsMiddleware;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
 $app = AppFactory::create();
+$app->add(new CorsMiddleware([
+    "origin" => ["*"], // Permite a todos los orígenes. Cambia esto para limitarlo a dominios específicos.
+    "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    "headers.allow" => ["Authorization", "If-Match", "If-Unmodified-Since", "Content-Type"],
+    "headers.expose" => ["Authorization", "Etag"],
+    "credentials" => true,
+    "cache" => 86400, // Establece la cabecera Access-Control-Max-Age en 86400 segundos = 24 horas
+]));
+
+$app->setBasePath('/api');
 
 require_once  __DIR__ . '/../controller/courses.php';
 
