c2ebd0ca7eeeaedc535a8a1ce04529582eeb388e
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2023-08-09 18:17:18 +0200

    Update course controller

--- controller/courses.php
+++ controller/courses.php
@@ -1,27 +1,68 @@
 <?php
 
+global $app;
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-$app->get('/courses', function (Request $request, Response $response, $args) {
+$controllerPrincipalUrl = "/courses";
+$data = [
+        [
+            'id' => 1,
+            'name' => 'Angular',
+            'description' => 'Angular is a platform for building mobile and desktop web applications.',
+            'price' => 200,
+            'imageUrl' => 'https://repository-images.githubusercontent.com/24195339/87018c00-694b-11e9-8b5f-c34826306d36',
+            'isAvailable' => true,
+            'category' => 'WEB'
+        ],
+        [
+            'id' => 2,
+            'name' => 'React',
+            'description' => 'React is a JavaScript library for building user interfaces.',
+            'price' => 150,
+            'imageUrl' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+            'isAvailable' => true,
+            'category' => 'WEB'
+        ],
+        [
+            'id' => 3,
+            'name' => 'Angular',
+            'description' => 'Angular is a platform for building mobile and desktop web applications.',
+            'price' => 200,
+            'imageUrl' => 'https://repository-images.githubusercontent.com/24195339/87018c00-694b-11e9-8b5f-c34826306d36',
+            'isAvailable' => true,
+            'category' => 'WEB'
+        ],
+        [
+            'id' => 4,
+            'name' => 'React',
+            'description' => 'React is a JavaScript library for building user interfaces.',
+            'price' => 150,
+            'imageUrl' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+            'isAvailable' => true,
+            'category' => 'WEB'
+        ],
+        [
+            'id' => 5,
+            'name' => 'React',
+            'description' => 'React is a JavaScript library for building user interfaces.',
+            'price' => 150,
+            'imageUrl' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+            'isAvailable' => true,
+            'category' => 'WEB'
+        ]
+    ];
 
+$app->get($controllerPrincipalUrl . '/all', function (Request $request, Response $response, $args) {
+    //error_log("Courses list");
     // Get request parameters
+
+    global $data;
     $params = $request->getQueryParams();
     //$hola = $params['hola'];
-    //echo $hola;
 
-    // Sample data to be converted to JSON
-    $data = array(
-        'name' => 'John Doe',
-        'age' => 30,
-        'email' => 'john.doe@example.com',
-        'is_subscribed' => true,
-        'address' => array(
-            'street' => '123 Main Street',
-            'city' => 'New York',
-            'zip' => '10001'
-        )
-    );
+
     // Convert the associative array to a JSON string
     $jsonObject = json_encode($data);
 
